c29b72961a04c7753adbb795f0c2611c6793d1d3
Andrew Harrington <aharrington4@mail.greenriver.edu>
2019-04-05 14:38:43 -0400

    Added a modal to verify the user's purchase

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php
+/**
+ * Author: Andrew Harrington
+ * Date: 4/4/2019
+ * aharrington.greenriverdev.com/328/cupcakes/
+ */
+$flavors = array('grasshopper' => 'The Grasshopper',
+    'maple' => 'Whiskey Maple Bacon',
+    'carrot' => 'Carrot Walnut',
+    'caramel' => 'Salted Caramel Cupcake',
+    'velvet' => 'Red Velvet',
+    'lemon' => 'Lemon Drop',
+    'tiramisu' => 'Tiramisu');
+
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $errorFound = false;
-    $flavors = $_POST['flavors'];
+    $checkedFlavors = $_POST['flavors'];
 //verification
     //name
     if($_POST['name'] == ""){
@@ -24,19 +37,41 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         $errorFound = true;
         echo 'ERROR: Pick a Flavor<br>';
     }
+
+    //no errors found
     if(!$errorFound){
+        echo '
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>';
+        echo
+        '<div id ="modal" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
+              aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content"">';
+        echo "<p>Thank you, " . $_POST['name'] . ", for your Order!</p>";
+        $total = number_format(count($checkedFlavors)* 3.50, 2, '.', '');
+        echo '<p>Order Summary</p>';
+        echo '<ul>';
+        foreach ($checkedFlavors as $key => $value)
+        {
+            echo "<li>$flavors[$value]</li>";
+        }
+        echo '</ul>';
+        echo "<p> Order Total: $". $total;
+
+        echo
+        '<div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>';
+        echo '<script>$("#modal").modal("show");</script>';
 
     }
 }
-/**
- * Created by PhpStorm.
- * User: Andrew Harrington
- * Date: 4/4/2019
- * Time: 12:44 PM
- */
-
-
-
 ?>
 <!doctype html>
 <html lang="en">
@@ -48,26 +83,13 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     <title>Cupcake Fundraiser</title>
 </head>
 <body>
-    <form action="finish.php" method="POST">
+    <form action="index.php" method="POST">
         <h1>Cupcake Fundraiser</h1>
         <p>Your Name:</p>
         <input type="text" placeholder="Please input your name" name="name">
         <p>Cupcake Flavors:</p>
         <ul>
             <?php
-                /**
-                 * Created by PhpStorm.
-                 * User: Andrew Harrington
-                 * Date: 4/4/2019
-                 * Time: 11:14 AM
-                 */
-                $flavors = array('grasshopper' => 'The Grasshopper',
-                                    'maple' => 'Whiskey Maple Bacon',
-                                    'carrot' => 'Carrot Walnut',
-                                    'caramel' => 'Salted Caramel Cupcake',
-                                    'velvet' => 'Red Velvet',
-                                    'lemon' => 'Lemon Drop',
-                                    'tiramisu' => 'Tiramisu');
                 foreach ($flavors as $key => $value) {
                     echo "<li>
                         <input type=\"checkbox\" name=\"flavors[]\" id=\"$key\" value=\"$key\">
